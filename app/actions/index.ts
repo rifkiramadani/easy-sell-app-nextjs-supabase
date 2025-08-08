7bdf9e47846158f2b37d415ec5631a944512b6f2
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2025-08-08 21:19:49 +0700

    feat: adding validation with zod to form addProduct

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,12 +1,53 @@
 "use server"
 
-export const sellYourItemsAction = async (prevState: unknown, formData: FormData) => {
-    console.log(formData.get('name'))
+import z, { success } from "zod";
+const MAX_FILE_SIZE = 5000000;
+const ACCEPTED_IMAGE_TYPES = [
+    'image/jpeg',
+    'image/jpg',
+    'image/png',
+    'image/webp',
+];
 
-    // const result = Player.safeParse({ username: 42, xp: "100" });
-    // if (!result.success) {
-    //     result.error;   // ZodError instance
-    // } else {
-    //     result.data;    // { username: string; xp: number }
-    // }
+
+export const sellYourItemsAction = async (prevState: unknown, formData: FormData) => {
+    console.log(formData.get('name'));
+    console.log(formData.get('price'));
+    console.log(formData.get('description'));
+    console.log(formData.get('imageUrl'));
+
+    const schema = z.object({
+        name: z.string().min(5),
+        price: z.string().min(1),
+        description: z.string().min(5),
+        imageUrl: z
+            .any()
+            .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+            .refine(
+                (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+                'Only .jpg, .jpeg, .png and .webp formats are supported.'
+            ),
+    })
+
+    const validationFields = schema.safeParse({
+        name: formData.get('name'),
+        price: formData.get('price'),
+        description: formData.get('description'),
+        imageUrl: formData.get('imageUrl')
+    });
+
+    if (!validationFields.success) {
+
+        console.log('error', validationFields.error)
+
+        return {
+            type: 'error',
+            error: validationFields.error.flatten().fieldErrors,
+            message: "Missing Fields, Failed to create Product"
+        }  // ZodError instance
+    }
+
+    return {
+        type: 'success'
+    }
 }
--- app/product/addProduct/page.tsx
+++ app/product/addProduct/page.tsx
@@ -15,6 +15,7 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { stat } from 'fs'
 
 const AddProduct = () => {
 
@@ -23,32 +24,47 @@ const AddProduct = () => {
     return (
         <div className='container mx-auto py-15 px-45'>
             <div className='flex justify-between items-center'>
-                <div className='w-sm'>
+                <div className='w-sm animate__animated animate__fadeInLeft'>
                     <span className='text-6xl font-bold bg-linear-to-bl from-sky-500 to-violet-300 text-transparent bg-clip-text'>Sell Your Items</span>
                     <p className='text-2xl mt-4'>Enter details of your items to start selling your items </p>
                 </div>
-                <div className='ms-20'>
-                    <UICard className='w-3xl h-110'>
+                <div className='ms-20 animate__animated animate__fadeInRight'>
+                    <UICard className='w-3xl h-auto'>
                         <CardHeader>
                             <CardTitle className='text-2xl'>Add Your Items!</CardTitle>
                         </CardHeader>
                         <CardContent>
+                            {state?.type == 'error' && (
+                                <span className='text-md font-light text-red-400'>{state.message}</span>
+                            )}
                             <form action={formAction}>
                                 <div className='mb-2'>
                                     <Label htmlFor='name' className='mb-2'>Name</Label>
                                     <Input id='name' type='text' name='name' />
+                                    {state?.error?.name && (
+                                        <span className='text-md font-light text-red-400'>{state.error.name}</span>
+                                    )}
                                 </div>
                                 <div className='mb-2'>
                                     <Label htmlFor='price' className='mb-2'>Price</Label>
                                     <Input id='price' type='number' name='price' />
+                                    {state?.error?.price && (
+                                        <span className='text-md font-light text-red-400'>{state.error.price}</span>
+                                    )}
                                 </div>
                                 <div className='mb-2'>
                                     <Label htmlFor='description' className='mb-2'>Description</Label>
                                     <Input id='description' type='text' name='description' />
+                                    {state?.error?.description && (
+                                        <span className='text-md font-light text-red-400'>{state.error.description}</span>
+                                    )}
                                 </div>
                                 <div className='mb-3'>
                                     <Label htmlFor='image' className='mb-2'>Image</Label>
-                                    <Input id='image' type='file' name='image' />
+                                    <Input id='image' type='file' name='imageUrl' />
+                                    {state?.error?.imageUrl && (
+                                        <span className='text-md font-light text-red-400'>{state.error.imageUrl}</span>
+                                    )}
                                 </div>
                                 <Button type='submit' className='bg-linear-to-bl from-sky-500 to-violet-300 hover:text-white hover:scale-110'>Add+</Button>
                             </form>
